85d66f48e994a53314d20fa582b70f6a08ef5f20
Majkell Veizaj <majkellveizaj@gmail.com>
2024-09-03 20:20:46 +0200

    feature production condition
    feature env
    
    feature env json key
    
    feature env json key
    
    feature env json key
    
    feature env json key
    
    feature env json key

--- football_match_predictor/features/feature_pipeline.py
+++ football_match_predictor/features/feature_pipeline.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import pickle
 
 from football_match_predictor.features.data_provider import (load_football_data, preprocess_data,
@@ -10,6 +11,8 @@ from football_match_predictor.util.parameter_retriever import get_parameters
 
 logger = Logger(name="feature_pipeline", level=logging.DEBUG)
 
+environment = os.getenv('ENVIRONMENT')
+
 
 def main(start_year: int, end_year: int):
 	df = load_football_data(start_year, end_year)
@@ -19,10 +22,11 @@ def main(start_year: int, end_year: int):
 	data_hash = generate_hash(start_year, end_year)
 	versioned_dir = f'data/{data_hash}/'
 
-	with open(f'{versioned_dir}features.pickle', 'wb') as f:
-		pickle.dump(features, f)
-	with open(f'{versioned_dir}target.pickle', 'wb') as f:
-		pickle.dump(target, f)
+	if environment != 'production':
+		with open(f'{versioned_dir}features.pickle', 'wb') as f:
+			pickle.dump(features, f)
+		with open(f'{versioned_dir}target.pickle', 'wb') as f:
+			pickle.dump(target, f)
 
 	upload_to_gcs(f'{versioned_dir}features.pickle', f'{versioned_dir}features.pickle')
 	upload_to_gcs(f'{versioned_dir}target.pickle', f'{versioned_dir}target.pickle')
