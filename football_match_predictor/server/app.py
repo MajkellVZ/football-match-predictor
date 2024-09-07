955b65a188b8c6d2552ddd4355c4dd68887c3b0a
Majkell Veizaj <majkellveizaj@gmail.com>
2024-09-08 00:07:06 +0200

    handle cors
    handle cors

--- football_match_predictor/server/app.py
+++ football_match_predictor/server/app.py
@@ -1,11 +1,13 @@
 import logging
 
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pandas as pd
 from football_match_predictor.model.predict import Predictor
 from football_match_predictor.util.logger import Logger
 
 app = Flask(__name__)
+CORS(app)
 logger = Logger(name="predictor", level=logging.DEBUG)
 
 predictor = Predictor('models/model.pickle')
